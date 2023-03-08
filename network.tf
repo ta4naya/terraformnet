1defaa2df41ad11eeb64c2454ab7d0b17eb954f3
Anthony <ta4naya@yahoo.com>
2023-03-08 20:23:21 +0100

    to

--- network.tf
+++ network.tf
@@ -11,12 +11,6 @@ resource "azurerm_subnet" "sycor" {
   virtual_network_name = azurerm_virtual_network.sycor.name
   address_prefixes     = ["10.0.2.0/24"]
 }
-resource "azurerm_subnet" "sidcor" {
-  name                 = "internal1"
-  resource_group_name  = azurerm_resource_group.sycor.name
-  virtual_network_name = azurerm_virtual_network.sycor.name
-  address_prefixes     = ["10.0.3.0/24"]
-}
 
 resource "azurerm_network_interface" "sycor" {
   count               = length(var.vmnic)
@@ -31,17 +25,6 @@ resource "azurerm_network_interface" "sycor" {
   }
 }
 
-resource "azurerm_network_interface" "sidcor" {
-  count               = length(var.vmnic2)
-  name                = var.vmnic2[count.index]
-  location            = azurerm_resource_group.sycor.location
-  resource_group_name = azurerm_resource_group.sycor.name
 
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.sidcor.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
 
  
