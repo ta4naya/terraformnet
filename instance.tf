032de7e83412eedb76ab0ac116d74bee84ada270
Anthony <ta4naya@yahoo.com>
2023-03-08 18:09:22 +0100

    second commit

--- instance.tf
+++ instance.tf
@@ -20,25 +20,4 @@ resource "azurerm_windows_virtual_machine" "sycor" {
   }
 }
 
-resource "azurerm_windows_virtual_machine" "sidcor" {
-  count                 = length(var.vmname1)
-  name                  = var.vmname1[count.index]
-  resource_group_name   = azurerm_resource_group.sycor.name
-  location              = azurerm_resource_group.sycor.location
-  size                  = "Standard_F2"
-  admin_username        = var.admin_login1
-  admin_password        = var.admin_password2
-  network_interface_ids = [azurerm_network_interface.sidcor[count.index].id, ]
-  os_disk {
-    name                 = "${var.vmname1[count.index]}-os-disk-01"
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-  source_image_reference {
-    publisher = var.image_publisher
-    offer     = var.image_offer
-    sku       = var.image_sku
-    version   = "latest"
-  }
-}
 
--- var.tf
+++ var.tf
@@ -3,29 +3,15 @@ variable "vmname" {
   description = "The name of the virtual machine"
   default     = ["sycor1", "sycor2"]
 }
-variable "vmname1" {
-  type        = list(string)
-  description = "The name of the virtual machine"
-  default     = ["sidcor1", "sidcor2"]
-}
+
 variable "vmnic" {
   type        = list(string)
   description = "The nics of vms"
   default     = ["sycor-nic", "sycor-nic2"]
 
 }
-variable "vmnic2" {
-  type        = list(string)
-  description = "The nics of vms"
-  default     = ["sidcor-nic", "sidcor-nic2"]
-}
+
 variable "admin_login" {
-  type        = string
-  description = "local admin user of the virtual machine"
-
-}
-
-variable "admin_login1" {
   type        = string
   description = "local admin user of the virtual machine"
 
@@ -37,11 +23,6 @@ variable "admin_password" {
 
 }
 
-variable "admin_password2" {
-  type        = string
-  description = "password of the local admin user"
-
-}
 variable "image_publisher" {
   type        = string
   description = "Azure image publisher"
