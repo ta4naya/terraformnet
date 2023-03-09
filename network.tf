a8ad3f9d9b27de3cffce0ef07bf71532066bd912
Anthony <ta4naya@yahoo.com>
2023-03-09 06:37:36 +0100

    last test§

--- network.tf
+++ network.tf
@@ -42,7 +42,7 @@ resource "azurerm_network_interface_security_group_association" "sycor" {
   name                = "pip-${count.index}"
   resource_group_name = azurerm_resource_group.sycor.name
   location            = azurerm_resource_group.sycor.location
-  allocation_method   = "dynamic"
+  allocation_method   = "Dynamic"
 
   tags = {
     environment = "Production"
